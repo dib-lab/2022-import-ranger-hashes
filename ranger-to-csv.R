41f595da1aae0ac6d6538fe6a879b572fe5506ce
C. Titus Brown <titus@idyll.org>
2022-09-14 11:33:22 -0700

    add argument parsing

--- ranger-to-csv.R
+++ ranger-to-csv.R
@@ -1,4 +1,13 @@
 #! /usr/bin/env Rscript
-x <- readRDS('/group/ctbrowngrp2/tereiter/github/2020-ibd/outputs/optimal_rf_seed/iHMP_optimal_rf_seed1.RDS')
-y <- attributes(x$variable.importance)
-write.csv(y, 'titus.csv')
+args = commandArgs(trailingOnly=TRUE)
+
+if (length(args)==0) {
+  stop("At least two arguments must be supplied - RDS file and output CSV", call.=FALSE)
+}
+      
+# e.g. '/group/ctbrowngrp2/tereiter/github/2020-ibd/outputs/optimal_rf_seed/iHMP_optimal_rf_seed1.RDS')
+
+model <- readRDS(args[1])
+
+hashes <- attributes(model$variable.importance)
+write.csv(hashes, args[2])
